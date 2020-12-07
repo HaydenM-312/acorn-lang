aea264de95fa578ef9cb2a3c23898e8524fd0761
HaydenM-312 <hjrmg565@gmail.com>
2020-12-07 16:06:07 -0500

    Finished tokenizer

--- amain.c
+++ amain.c
@@ -1,9 +1,31 @@
 #include "main.h"
 
 int main(int argc, char* argv[]) {
-	char* 	file = load_txt(argv[1]); // Load the file named in the first argument into an array
-	Token* 	tokens = tokenize(file); // Turn the array into a list of tokens
+		if (argc > 1) {
+		char* 	file = load_txt(argv[1]); // Load the file named in the first argument into an array
+		Token* 	tokens = tokenize(file); // Turn the array into a list of tokens
 
-	free(file); // Free the malloc'ed file
+		for(int i = 0; tokens[i-1].type != TOKEN_EOF; i++) {
+			printf("%d: ", i);
+			print_token(tokens[i]);
+			free_token(&tokens[i]);
+		}
+
+		free(tokens);
+		free(file); // Free the malloc'ed file
+	} else {
+		while (1) {
+			char in_text[1024];
+			printf("> ");
+			fgets(in_text,1024,stdin);
+			Token* tokens = tokenize(in_text); // Turn the array into a list of tokens
+
+			for(int i = 0; tokens[i-1].type != TOKEN_EOF; i++) {
+				printf("%d: ", i);
+				print_token(tokens[i]);
+				free_token(&tokens[i]);
+			}
+		}
+	}
 	return 0;
 }
--- atokenize.c
+++ atokenize.c
@@ -2,11 +2,7 @@
 #include "main.h"
 #include <ctype.h>
 
-struct Tokens{
-	int type;
-	long line;
-	char* value;
-};
+size_t i;
 
 enum Token_Types {
 	TOKEN_PLUS,
@@ -26,36 +22,67 @@ enum Token_Types {
 	TOKEN_RPAREN,
 	TOKEN_LBRACKET,
 	TOKEN_RBRACKET,
-	TOKEN_DOUBLE,
-	TOKEN_INT,
+	TOKEN_NAME,
+	TOKEN_NUM,
 	TOKEN_STRING,
 	TOKEN_IF,
 	TOKEN_THEN,
 	TOKEN_ELIF,
 	TOKEN_ELSE,
 	TOKEN_LET,
+	TOKEN_SET,
 	TOKEN_NIL,
-	TOKEN_EOF
+	TOKEN_CASE,
+	TOKEN_MATCH,
+	TOKEN_ARROW,
+	TOKEN_DO,
+	TOKEN_EOF,
+	TOKEN_SOF
+};
+char* Token_Types_Print[] = {
+	"TOKEN_PLUS",
+	"TOKEN_MINUS",
+	"TOKEN_TIMES",
+	"TOKEN_DIV",
+	"TOKEN_EQU",
+	"TOKEN_GTE",
+	"TOKEN_NULLJOINER",
+	"TOKEN_GTEQU",
+	"TOKEN_MOD",
+	"TOKEN_LTE",
+	"TOKEN_LTEQU",
+	"TOKEN_NEQ",
+	"TOKEN_RANGE",
+	"TOKEN_LPAREN",
+	"TOKEN_RPAREN",
+	"TOKEN_LBRACKET",
+	"TOKEN_RBRACKET",
+	"TOKEN_NAME",
+	"TOKEN_NUM",
+	"TOKEN_STRING",
+	"TOKEN_IF",
+	"TOKEN_THEN",
+	"TOKEN_ELIF",
+	"TOKEN_ELSE",
+	"TOKEN_LET",
+	"TOKEN_SET",
+	"TOKEN_NIL",
+	"TOKEN_CASE",
+	"TOKEN_MATCH",
+	"TOKEN_ARROW",
+	"TOKEN_DO",
+	"TOKEN_EOF",
+	"TOKEN_SOF"
 };
 
-char peek(size_t index, char array[]) {
-	return array[index + 1];
-}
-
-char consume(size_t* index, char array[]) {
-	return array[(*index)++];
-}
-
-int isalphasymbolic(char c) {
-	return (isalpha(c) || c == '-' || c == '_');
-}
-
-int isdigitoid(char c) {
-	return isdigit(c) || c == '.';
-}
+struct Tokens {
+	int type;
+	long line;
+	char* value;
+};
 
 void print_token(Token t) {
-	printf("[type:%d, line:%d", t.type, t.line);
+	printf("[type:%s", Token_Types_Print[t.type]);
 	if (t.value != NULL) {
 		printf(", val:%s]\n", t.value);
 	} else {
@@ -74,16 +101,218 @@ void init_token(Token *t, int new_type, long new_line, char new_value[]) {
 	}
 }
 
+Token new_token(int new_type, long new_line, char new_value[]) {
+	Token t;
+	t.type = new_type;
+	t.line = new_line;
+	if (new_value != NULL) {
+		t.value = malloc(strlen(new_value) + 1);
+		t.value = new_value;
+	} else {
+		t.value = NULL;
+	}
+	return t;
+}
+
 void free_token(Token *t) {
 	free(t->value);
 }
-Token* tokenize(char text[]) {
-	Token* token_list;
 
-	for(size_t i = 0; i < strlen(text); i++) {
-		switch (text[i]) {
-			default:break;
+char peek(char array[]) {
+	return array[i + 1];
+}
+int isalphasymbolic(char c) {
+	return (isalpha(c) ||  c == '_');
+}
+
+int isdigitoid(char c) {
+	return isdigit(c) || c == '.';
+}
+
+
+Token* tokenize(char text[]) {
+	Token* token_array = malloc(sizeof(Token));
+	token_array[0] = new_token(TOKEN_SOF, 0, '\0');
+	long token_count = 1;
+	long cur_line = 1;
+	for(i = 0; i <= strlen(text); i++) {
+		switch(text[i]) {
+			// Misc operators
+			case 0:
+				token_count++;
+				token_array = realloc(token_array, sizeof(Token) * token_count);
+				token_array[token_count-1] = new_token(TOKEN_EOF, cur_line, '\0');
+				break;
+			case '\n':
+				cur_line++;
+				break;
+			case ';':
+				while (text[i] != '\n') i++;
+				break;
+			case '"': {
+				size_t j = i+1;
+				i++; while(text[i] != '"') i++;  // Find the size of the number
+																// It makes no sense why decrementing i is necesarry here, but who cares if it works
+				char* out = malloc(i-j);		//
+				size_t sze = i-j;					//
+				for(size_t k = 0; k < sze; k++) {
+					out[k] = text[j];
+					j++;
+				}
+
+				out[sze] = '\0';
+				token_count++;
+				token_array = realloc(token_array, sizeof(Token) * token_count);
+				token_array[token_count-1] = new_token(TOKEN_STRING, cur_line, out);
+				break;
+			}
+			case '|':
+				if (peek(text) == '>') {
+					token_count++;
+					token_array = realloc(token_array, sizeof(Token) * token_count);
+					token_array[token_count-1] = new_token(TOKEN_NULLJOINER, cur_line, '\0');
+					i++;
+				}
+				break;
+			// Basic math operators
+			case '%':
+				token_count++;
+				token_array = realloc(token_array, sizeof(Token) * token_count);
+				token_array[token_count-1] = new_token(TOKEN_MOD, cur_line, '\0');
+				break;
+			case '+':
+				token_count++;
+				token_array = realloc(token_array, sizeof(Token) * token_count);
+				token_array[token_count-1] = new_token(TOKEN_PLUS, cur_line, '\0');
+				break;
+			case '-':
+				token_count++;
+				token_array = realloc(token_array, sizeof(Token) * token_count);
+				if (peek(text) == '>') {
+					token_array[token_count-1] = new_token(TOKEN_ARROW, cur_line, '\0');
+					i++;
+				} else {
+					token_array[token_count-1] = new_token(TOKEN_MINUS, cur_line, '\0');
+				}
+				break;
+			case '*':
+				token_count++;
+				token_array = realloc(token_array, sizeof(Token) * token_count);
+				token_array[token_count-1] = new_token(TOKEN_TIMES, cur_line, '\0');
+				break;
+			case '.':
+				if (peek(text) == '.') {
+					token_count++;
+					token_array = realloc(token_array, sizeof(Token) * token_count);
+					token_array[token_count-1] = new_token(TOKEN_RANGE, cur_line, '\0');
+					i++;
+				}
+				break;
+			// Brackets
+			case '(':
+				token_count++;
+				token_array = realloc(token_array, sizeof(Token) * token_count);
+				token_array[token_count-1] = new_token(TOKEN_LPAREN, cur_line, '\0');
+				break;
+			case ')':
+				token_count++;
+				token_array = realloc(token_array, sizeof(Token) * token_count);
+				token_array[token_count-1] = new_token(TOKEN_RPAREN, cur_line, '\0');
+				break;
+			case '[':
+				token_count++;
+				token_array = realloc(token_array, sizeof(Token) * token_count);
+				token_array[token_count-1] = new_token(TOKEN_LBRACKET, cur_line, '\0');
+				break;
+			case ']':
+				token_count++;
+				token_array = realloc(token_array, sizeof(Token) * token_count);
+				token_array[token_count-1] = new_token(TOKEN_RBRACKET, cur_line, '\0');
+				break;
+			case '/':
+				token_count++;
+				token_array = realloc(token_array, sizeof(Token) * token_count);
+				if (peek(text) == '=') {
+					token_array[token_count-1] = new_token(TOKEN_NEQ, cur_line, '\0');
+					i++;
+				}
+				else {
+					token_array[token_count-1] = new_token(TOKEN_DIV, cur_line, '\0');
+				}
+				break;
+			// Comparison operators
+			case '=':
+				token_count++;
+				token_array = realloc(token_array, sizeof(Token) * token_count);
+				token_array[token_count-1] = new_token(TOKEN_EQU, cur_line, '\0');
+				break;
+			case '>':
+				token_count++;
+				token_array = realloc(token_array, sizeof(Token) * token_count);
+				if (peek(text) == '=') {
+					token_array[token_count-1] = new_token(TOKEN_GTEQU, cur_line, '\0');
+					i++;
+				}
+				else {
+					token_array[token_count-1] = new_token(TOKEN_GTE, cur_line, '\0');
+				}
+				break;
+			case '<':
+				token_count++;
+				token_array = realloc(token_array, sizeof(Token) * token_count);
+				if (peek(text) == '=') {
+						token_array[token_count-1] = new_token(TOKEN_LTEQU, cur_line, '\0');
+					i++;
+				}
+				else {
+					token_array[token_count-1] = new_token(TOKEN_LTE, cur_line, '\0');
+				}
+				break;
+			default:
+				if (isalphasymbolic(text[i])) {
+					size_t j = i;
+					while((isalphasymbolic(text[i]))) i++; // Find the size of the text
+					i--;
+					char* out = malloc(i-j);
+					size_t sze = i-j+1;
+					for(size_t k = 0; k < sze; k++) {
+						out[k] = text[j];
+						j++;
+					}
+					out[sze] = '\0';
+					token_count++;
+					token_array = realloc(token_array, sizeof(Token) * token_count);
+						if (strcmp(out, "let") == 0) {token_array[token_count-1] = new_token(TOKEN_LET, cur_line, '\0');}
+						else if (strcmp(out, "set") == 0) {token_array[token_count-1] = new_token(TOKEN_SET, cur_line, '\0');}
+						else if (strcmp(out, "if") == 0) {token_array[token_count-1] = new_token(TOKEN_IF, cur_line, '\0');}
+						else if (strcmp(out, "elif") == 0) {token_array[token_count-1] = new_token(TOKEN_ELIF, cur_line, '\0');}
+						else if (strcmp(out, "else") == 0) {token_array[token_count-1] = new_token(TOKEN_ELSE, cur_line, '\0');}
+						else if (strcmp(out, "then") == 0) {token_array[token_count-1] = new_token(TOKEN_THEN, cur_line, '\0');}
+						else if (strcmp(out, "let") == 0) {token_array[token_count-1] = new_token(TOKEN_LET, cur_line, '\0');}
+						else if (strcmp(out, "case") == 0) {token_array[token_count-1] = new_token(TOKEN_CASE, cur_line, '\0');}
+						else if (strcmp(out, "match") == 0) {token_array[token_count-1] = new_token(TOKEN_MATCH, cur_line, '\0');}
+						else if (strcmp(out, "do") == 0) {token_array[token_count-1] = new_token(TOKEN_DO, cur_line, '\0');}
+						else if (strcmp(out, "end") == 0 || strcmp(out, "nil") == 0) { token_array[token_count-1] = new_token(TOKEN_NIL, cur_line, '\0');}
+						else {token_array[token_count-1] = new_token(TOKEN_NAME, cur_line, out);}
+					break;
+				} else if (isdigit(text[i])) {
+					size_t j = i;
+					while(isdigit(text[i]) || (text[i] == '.' && peek(text) != '.')) i++;  // Find the size of the number
+					i--;												// It makes no sense why decrementing i is necesarry here, but who cares if it works
+					char* out = malloc(i-j);		//
+					size_t sze = i-j+1;					//
+					for(size_t k = 0; k < sze; k++) {
+						out[k] = text[j];
+						j++;
+					}
+
+					out[sze] = '\0';
+					token_count++;
+					token_array = realloc(token_array, sizeof(Token) * token_count);
+					token_array[token_count-1] = new_token(TOKEN_NUM, cur_line, out);
+					break;
+				}
 		}
 	}
-	return token_list;
+	return token_array;
 }
