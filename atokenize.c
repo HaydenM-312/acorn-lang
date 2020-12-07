7b3cfa6e0dab9aa41990d8c6f340543143744706
HaydenM-312 <hjrmg565@gmail.com>
2020-12-06 19:38:07 -0500

    Added token datatype

--- atokenize.c
+++ atokenize.c
@@ -2,6 +2,42 @@
 #include "main.h"
 #include <ctype.h>
 
+typedef struct {
+	int type;
+	long line;
+	char* value;
+} Token;
+
+enum Token_Types {
+	TOKEN_PLUS,
+	TOKEN_MINUS,
+	TOKEN_TIMES,
+	TOKEN_DIV,
+	TOKEN_EQU,
+	TOKEN_GTE,
+	TOKEN_NULLJOINER,
+	TOKEN_GTEQU,
+	TOKEN_MOD,
+	TOKEN_LTE,
+	TOKEN_LTEQU,
+	TOKEN_NEQ,
+	TOKEN_RANGE,
+	TOKEN_LPAREN,
+	TOKEN_RPAREN,
+	TOKEN_LBRACKET,
+	TOKEN_RBRACKET,
+	TOKEN_DOUBLE,
+	TOKEN_INT,
+	TOKEN_STRING,
+	TOKEN_IF,
+	TOKEN_THEN,
+	TOKEN_ELIF,
+	TOKEN_ELSE,
+	TOKEN_LET,
+	TOKEN_NIL,
+	TOKEN_EOF
+};
+
 char peek(size_t index, char array[]) {
 	return array[index + 1];
 }
@@ -17,74 +53,39 @@ int isalphasymbolic(char c) {
 int isdigitoid(char c) {
 	return isdigit(c) || c == '.';
 }
-// This is an incomplete function to do something with a token, in the future this will 
-// add to the token list.
-void token(char* t, char*** list) {
-	printf("%s\n",t); 
+
+void print_token(Token t) {
+	printf("[type:%d, line:%d", t.type, t.line);
+	if (t.value != NULL) {
+		printf(", val:%s]\n", t.value);
+	} else {
+		printf(", val:NULL]\n");
+	}
 }
 
-
+void init_token(Token *t, int new_type, long new_line, char new_value[]) {
+	t->type = new_type;
+	t->line = new_line;
+	if (new_value != NULL) {
+		t->value = malloc(strlen(new_value));
+		t->value = new_value;
+	} else {
+		t->value = NULL;
+	}
+}
 char** tokenize(char text[]) {
 	char** token_list;
 
 	for(size_t i = 0; i < strlen(text); i++) {
 		switch (text[i]) {
-			case ';':
-				while(consume(&i, text) != '\n') {}
-				break;
-			case '`':
-				consume(&i, text);
-				while(consume(&i, text) != '`'){}
-				break;
-			case '+':
-				token("+", &token_list);
-				break; 
-			case '-':
-				token("-", &token_list);
-				break; 
-			case '*':
-				token("*", &token_list);
-				break; 
-			case '=':
-				token("=", &token_list);
-				break; 
-			case '%':
-				token("%", &token_list);
-				break; 
-			case ':':
-				token(":", &token_list);
-				break; 
-			case '>':
-				if (peek(i, text) == '=') {
-					consume(&i, text);
-					token(">=", &token_list);
-				}
-				else {
-					token(">", &token_list);
-				}
-				break;
-			case '<':
-				if (peek(i, text) == '=') {
-					consume(&i, text);
-					token("<=", &token_list);
-				}
-				else {
-					token("<", &token_list);
-				}
-				break;
-			case '/':
-				if (peek(i, text) == '=') {
-					consume(&i, text);
-					token("/=", &token_list);
-				}
-				else {
-					token("/", &token_list);
-				}
-				break;
-			default:
-				
-				break; 
+			default:break;
 		}
 	}
+	Token v, d;
+	init_token(&v, TOKEN_STRING, 0, "Hello, friends");
+	print_token(v);
+	init_token(&d, TOKEN_DIV, 0, NULL);
+	print_token(d);
+
 	return token_list;
 }
